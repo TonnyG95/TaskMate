647af04681c3b7a9b002a07255eeae91572e50c5
TonnyG95 <galic_antonio@hotmail.com>
2022-05-01 21:47:05 +0000

    finish with CRUD

--- todo_app/views.py
+++ todo_app/views.py
@@ -37,3 +37,31 @@ def contact(request):
         'contact_text': 'Contact us',
     }
     return render(request, 'todo_app/contact.html', context)
+
+
+def edit_task(request, task_id):
+     if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance= task)
+        if form.is_valid():
+            form.save()
+
+        messages.success(request, ("Task Edited!"))
+        return redirect('todolist')
+     else:
+        task_obj = TaskList.objects.get(pk=task_id)
+        return render(request, 'todo_app/edit.html', {'task_obj': task_obj})
+
+
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True 
+    task.save()
+    return redirect('todolist')
+
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False 
+    task.save()
+    return redirect('todolist')
