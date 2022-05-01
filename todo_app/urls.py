63e6719018798883f9a32067665260cac16c71b0
TonnyG95 <galic_antonio@hotmail.com>
2022-05-01 20:07:10 +0000

    added delete function to tasks

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('tasks/', views.todolist, name='todolist'),
+    path('delete/<task_id>', views.delete_task, name='delete_task'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from todo_app.models import TaskList
 from todo_app.forms import TaskForm
+from django.contrib import messages
 
 # Create your views here.
+
 
 def todolist(request):
 
@@ -10,20 +12,28 @@ def todolist(request):
         form = TaskForm(request.POST or None)
         if form.is_valid():
             form.save()
+        messages.success(request, ("New task added!"))
         return redirect('todolist')
     else:
         all_tasks = TaskList.objects.all
-        return render(request, 'todo_app/todo.html', {'all_tasks':all_tasks})
+        return render(request, 'todo_app/todo.html', {'all_tasks': all_tasks})
+
+
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todolist')
 
 
 def about(request):
     context = {
         'about_text': 'About us',
-        }
-    return render(request, 'todo_app/about.html', context )
+    }
+    return render(request, 'todo_app/about.html', context)
+
 
 def contact(request):
     context = {
         'contact_text': 'Contact us',
-        }
-    return render(request, 'todo_app/contact.html', context )
+    }
+    return render(request, 'todo_app/contact.html', context)
