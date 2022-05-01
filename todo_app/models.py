226b954601f08e086646c0b8c179414ad326c5f4
TonnyG95 <galic_antonio@hotmail.com>
2022-05-01 17:35:36 +0000

    added task db

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class TaskList(models.Model):
+    task = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task + " | " + "Done: " + str(self.done) + " | " + " important: " + str(self.important)
+    
