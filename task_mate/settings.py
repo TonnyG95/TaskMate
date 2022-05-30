0f9485ace9b824093d50d9f4f72aee69e33196e1
TonnyG95 <galic_antonio@hotmail.com>
2022-05-30 16:21:30 +0000

    impored crispy forms and filter registration form to use crispy form

--- task_mate/settings.py
+++ task_mate/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'todo_app',
     'users_app',
+    'crispy_forms',
     
 ]
 
@@ -148,3 +149,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
