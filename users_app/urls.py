16a26b2d1132b60e2eec5f1c87923a556e2c1a54
TonnyG95 <galic_antonio@hotmail.com>
2022-05-30 17:04:46 +0000

    added logout option

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
 from users_app import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('register', views.register, name="register"),
+    path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name="todo_app/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="todo_app/logout.html"), name="logout"),
 
 ]
