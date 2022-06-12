9a679e726b834965a80030989f5aed06d1a545bf
TonnyG95 <galic_antonio@hotmail.com>
2022-06-12 13:00:37 +0000

    depolyment

--- task_mate/settings.py
+++ task_mate/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-task-mate.herokuapp.com', 'localhost', 'https://ci2-taskmate.herokuapp.com']
+ALLOWED_HOSTS = ['ci-task-mate.herokuapp.com', 'localhost', 'ci2-taskmate.herokuapp.com']
 
 
 # Application definition
